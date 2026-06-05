cb1e457a6adb75522cbd33bd31c30b4fefcf61eb
Durrell Gemuh <durrellgemuh07@gmail.com>
2026-06-05 20:10:08 +0100

    fix: use babel-jest transform to avoid SWC binary dependency in CI

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,15 +1,14 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  dir: "./",
-});
-
+/** @type {import('jest').Config} */
 const customJestConfig = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: ["/node_modules/"],
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = customJestConfig;
