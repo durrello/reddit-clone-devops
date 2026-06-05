bccbec6e672e845ce14663e1016f7a50b69f7034
Durrell Gemuh <durrellgemuh07@gmail.com>
2026-06-05 21:33:02 +0100

    feat: switch to AWS ECR registry, deploy to EKS cluster
    - Use ECR (185542457060.dkr.ecr.us-east-1.amazonaws.com/reddit-clone-dev)
    - Update K8s deployment to pull from ECR
    - Update CI/CD to build+push to ECR
    - EKS nodes have native IAM access to ECR (no image pull secrets needed)
    - Terraform updated to K8s 1.31

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ module "eks" {
   version = "19.21.0"
 
   cluster_name    = "${var.project_name}-${var.environment}"
-  cluster_version = "1.28"
+  cluster_version = "1.31"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
